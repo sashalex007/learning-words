acb8cf1d7530189e921893359f59b4b0131c38f3
Aloun Vangkeosay <alounv@gmail.com>
2023-06-03 10:53:28 +0200

    Format analytics

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { Data } from "@/stores/data";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { FC } from "react";
 
 export const Analytics: FC = () => {
   return (
-    <div>
-      <LettersAnalytics />
-      <DigramsAnalytics />
-      <TrigramsAnalytics />
-      <WordsAnalytics />
+    <div className="flex flex-col gap-4">
+      <div>
+        Left part is the target, right part are the errors (wrong letters typed)
+        and their associated frequencies (filter by date will be added soon).
+      </div>
+      <div className="flex flex-col gap-8">
+        <LettersAnalytics />
+        <DigramsAnalytics />
+        <TrigramsAnalytics />
+        <WordsAnalytics />
+      </div>
     </div>
   );
 };
@@ -36,10 +43,41 @@ interface IAnalyticsLayout {
 }
 
 const AnalyticsLayout: FC<IAnalyticsLayout> = ({ title, data }) => {
+  const color = useColorModeValue("gray.700", "gray.300");
+  const targets = Object.entries(data).map(([target, value]) => {
+    const count = Object.values(value).reduce((a, b) => a + b, 0);
+    return { target, value, count };
+  });
+  targets.sort((a, b) => b.count - a.count);
   return (
-    <div>
-      <div>{title}</div>
-      <div>{JSON.stringify(data)}</div>
+    <div className="flex flex-col gap-2">
+      <Box className="font-bold" color={color}>
+        {title}
+      </Box>
+      <ul>
+        {targets.map(({ target, value }) => (
+          <li key={target} className="flex gap-2 ml-4">
+            <Box color={color} className="text-lg">
+              {target}
+            </Box>
+            -
+            <ElementAnalytics {...value} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+const ElementAnalytics: FC<Record<string, number>> = (data) => (
+  <ul className="flex">
+    {Object.entries(data).map(([error, count], i) => {
+      const isLast = i === Object.entries(data).length - 1;
+      return (
+        <li className="ml-4" key={error}>{`${error} ${count}${
+          isLast ? "" : ","
+        }`}</li>
+      );
+    })}
+  </ul>
+);
